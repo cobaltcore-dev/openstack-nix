fcb612cb9f9663ad88e16dc8a017453c5ded203c
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-01-24 11:53:33 +0100

    tests: add meta.tag to integration tests

--- tests/default.nix
+++ tests/default.nix
@@ -2,6 +2,9 @@
   pkgs,
   nixosModules,
 }:
-{
-  openstack-default-setup = pkgs.callPackage ./openstack-default-setup.nix { inherit nixosModules; };
-}
+let
+  tests = {
+    openstack-default-setup = pkgs.callPackage ./openstack-default-setup.nix { inherit nixosModules; };
+  };
+in
+pkgs.lib.mapAttrs (_: v: pkgs.lib.recursiveUpdate v { meta.tag = "nix-integration-test"; }) tests
