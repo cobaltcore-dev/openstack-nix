d746edb816c4d8d3a2d0d32258b1c1214164e6bb
Paul Kroeher <paul.kroeher@cyberus-technology.de>
2026-06-30 11:48:04 +0200

    ci: increase volume attachemnt timeouts
    In github ci the test vm need >= 45 to boot.
    It's longer than in a local test.
    
    Signed-off-by: Paul Kroeher <paul.kroeher@cyberus-technology.de>
    On-behalf-of: SAP paul.kroeher@sap.com

--- tests/openstack-default-setup.nix
+++ tests/openstack-default-setup.nix
@@ -135,7 +135,7 @@ pkgs.testers.nixosTest {
 
       # Ping the OpenStack VM from the controller host. We use the network
       # namespace dedicated for the VM to ping it.
-      assert retry_until_succeed(controllerVM, f"ip netns exec {net_ns} ping -c 1 {vm_ip}", 30)
+      assert retry_until_succeed(controllerVM, f"ip netns exec {net_ns} ping -c 1 {vm_ip}", 120)
 
       # create volume with 4GB
       controllerVM.execute("openstack volume create --size 4 test_vol")
@@ -155,7 +155,7 @@ pkgs.testers.nixosTest {
       print(f"openstack volume show test_vol: {output}")
 
       # wait until volume is attached
-      assert retry_until_succeed(controllerVM, "openstack volume show test_vol -f value -c status | grep 'in-use'", 40)
+      assert retry_until_succeed(controllerVM, "openstack volume show test_vol -f value -c status | grep 'in-use'", 120)
 
       # add ssh host key to known_hosts
       retry_until_succeed(controllerVM, f"ip netns exec {net_ns} ssh-keyscan {vm_ip} > ~/.ssh/known_hosts", 60)
